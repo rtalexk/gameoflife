b57581c0e959cc81b8b2b65aed5bd71c78fad426
Alejandro Rivera <alejandro.rivera@whisper.sh>
2022-03-09 10:09:30 -0700

    feat: add Position and Styles global types

---
+++ src/types.ts
@@ -0,0 +1,22 @@
+
+export class Position {
+  private _x: number;
+  private _y: number;
+
+  constructor(x: number, y: number) {
+    this._x = x;
+    this._y = y;
+  }
+
+  get x() {
+    return this._x;
+  }
+
+  get y() {
+    return this._y;
+  }
+}
+
+export interface Styles extends React.CSSProperties {
+  [key: string]: any;
+}
